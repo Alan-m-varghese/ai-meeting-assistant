57fb219907d94bb91ae3de0f9550e98aa62caba3
Alan M Varghese <146580812+Alan-m-varghese@users.noreply.github.com>
2025-07-26 22:59:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const summaryRoutes = require('./routes/summaryRoutes');
-const { authRouter } = require('./googleAuth');
+const authRouter = require('./googleAuth');
 const app = express();
 app.use(cors());
 app.use(express.json());
